4abe5089503a51af35a74c09a5c8523e4a3f0a39
empavia <59708667+empavia@users.noreply.github.com>
2025-09-12 16:34:34 -0700

    UCM batch processing

---
+++ code/Urban_Cooling_Modeling_Runs/invest_setup.sh
@@ -0,0 +1,28 @@
+# This script will allow users to run
+# various scenarios of the Urban Cooling Model
+# for the Wellcome Trust Project.
+# to start, make sure you have an environment created with
+# the package by running: mamba create -n invest-env -c conda-forge natcap.invest
+# once complete, you will run: conda activate invest-env
+# next you can manually edit the model parameter files then run this
+# script in full: sh invest_setup.sh or the
+# code for each scenario below:
+
+# Make sure to cd into the following directory:
+cd ~/code/Urban_Cooling_Modeling_Runs/
+
+# runs all temp and uhi combos for current and future conditions
+# for the current lulc with tcc
+python execute_invest_urban_cooling_model_current_lulc.py
+
+# runs all temp and uhi combos for current and future conditions
+# for scenario 1
+python execute_invest_urban_cooling_model_scenario1.py
+
+# runs all temp and uhi combos for current and future conditions
+# for scenario 2
+python execute_invest_urban_cooling_model_scenario2.py
+
+# runs all temp and uhi combos for current and future conditions
+# for scenario 3
+python execute_invest_urban_cooling_model_scenario3.py
