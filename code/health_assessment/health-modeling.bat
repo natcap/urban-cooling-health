c5908d2736c5aa127b7ff1aeffc2425c5c173e48
Yingjie Li <yingjieli.edu@gmail.com>
2025-09-22 00:18:45 -0700

    update input data for simulation!

--- code/health_assessment/health-modeling.bat
+++ code/health_assessment/health-modeling.bat
@@ -4,15 +4,15 @@ echo.
 
 python D:/natcap/urban-cooling-health/code/health_assessment/health-modeling.py ^
     --t_baseline "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/current_lulc/current_climate/intermediate/T_air_london_current_scenario_20deg_2uhi.tif" ^
-    --t_scenario "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/current_lulc/current_climate/intermediate/T_air_london_current_scenario_25deg_2uhi.tif" ^
-    --align_to "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/gbr_pop_2021_CN_100m_R2025A_v1_EPSG27700_clip2aoi.tif" ^
-	--pop_baseline "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/gbr_pop_2021_CN_100m_R2025A_v1_EPSG27700_clip2aoi.tif" ^
-    --pop_scenario "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/gbr_pop_2030_CN_100m_R2025A_v1_EPSG27700_clip2aoi.tif" ^
-    --baseline_deaths_cardio "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_100m_bng/i00_i99_ix_diseases_of_the_circulatory_system_bng100m_2021.tif" ^
-    --baseline_deaths_resp   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_100m_bng/j00_j99_x_diseases_of_the_respiratory_system_bng100m_2021.tif" ^
-	--baseline_deaths_cere   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_100m_bng/j00_j99_x_diseases_of_the_respiratory_system_bng100m_2021.tif" ^
+    --t_scenario "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/scenario1/current_climate/intermediate/T_air_london_current_scenario_25deg_2uhi.tif" ^
+    --align_to "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/current_lulc/current_climate/intermediate/T_air_london_current_scenario_20deg_2uhi.tif" ^
+	--pop_baseline "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/resampled_10m/gbr_pop_2021_10m_areal.tif" ^
+    --pop_scenario "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/resampled_10m/gbr_pop_2021_10m_areal.tif" ^
+    --baseline_deaths_cardio "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/i00_i99_ix_diseases_of_the_circulatory_system_bng10m_2021.tif" ^
+    --baseline_deaths_resp   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/j00_j99_x_diseases_of_the_respiratory_system_bng10m_2021.tif" ^
+	--baseline_deaths_cere   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/f00_f99_v_mental_and_behavioural_disorders_bng10m_2021.tif" ^
     --out_dir "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/health_output" ^
-    --n_draws 1000
+    --n_draws 100
 
 echo.
 echo Analysis complete! Check the output directory for results.
