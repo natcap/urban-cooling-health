786a2cf34dc44d03c73309076b16d83633158419
empavia <epavia@stanford.com>
2025-11-04 12:20:55 -0800

    add temperature 28degC; run scenario4 10%

---
+++ code/Urban_Cooling_Modeling_Runs/invest_setup_scenario4.sh
@@ -0,0 +1,21 @@
+# This script will allow users to run
+# various scenarios of the Urban Cooling Model
+# for the Wellcome Trust Project.
+# to start, make sure you have an environment created with
+# the package by running: mamba create -n invest-env -c conda-forge natcap.invest
+# once complete, you will run: conda activate invest-env
+# next you can manually edit the model parameter files then run this
+# make sure you have your correct prefixes
+# as a first argument
+# script in full: sh invest_setup.sh or the
+# code for each scenario below:
+
+# Set prefix to GDrive or Oak path
+path_prefix = 'G:\Shared drives\Wellcome Trust Project Data'
+# Make sure to cd into the following directory:
+cd ~/code/Urban_Cooling_Modeling_Runs/
+
+# runs all temp and uhi combos for current and future conditions
+# for the scenario 4 10% increase (--eap flag will run work productivity and energy)
+python execute_invest_urban_cooling_model_scenario_10prc_TCC.py $path_prefix --eap
+
