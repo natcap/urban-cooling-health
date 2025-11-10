a3b310869b6b32dcdd6e57307a127414a0cb1e5a
Yingjie Li <yingjieli.edu@gmail.com>
2025-11-10 15:46:00 -0800

    refine the figures

---
+++ code/func_colors.R
@@ -0,0 +1,10 @@
+
+
+scenario_colors = c(
+  "scenario0" = "gray20", 
+  "scenario1" = "#d95f0e", # #78C679
+  "scenario2_TR" = "#fec44f", # tree risk
+  "scenario3_TO" = "#d9f0a3", # tree opportunity
+  "scenario4_10" = "#78c679",
+  "scenario4_20" = "#238443",
+  "scenario4_30" = "#004529")
