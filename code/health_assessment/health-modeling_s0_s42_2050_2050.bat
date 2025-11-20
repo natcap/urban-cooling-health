f1742cbe79a23b19223b8b27b67c1aa6d86cd76d
Yingjie Li <yingjieli.edu@gmail.com>
2025-11-19 17:38:45 -0800

    minor edit on var name

--- code/health_assessment/health-modeling_s0_s42_2050_2050.bat
+++ code/health_assessment/health-modeling_s0_s42_2050_2050.bat
@@ -10,7 +10,7 @@ python D:/natcap/urban-cooling-health/code/health_assessment/health-modeling.py 
     --pop_scenario "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/pop_raster/resampled_10m/gbr_pop_2021_10m_areal.tif" ^
     --baseline_deaths_cardio "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/i00_i99_ix_diseases_of_the_circulatory_system_bng10m_2021.tif" ^
     --baseline_deaths_resp   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/j00_j99_x_diseases_of_the_respiratory_system_bng10m_2021.tif" ^
-	--baseline_deaths_cere   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/x60_x84_intentional_self_harm_bng10m_2021.tif" ^
+	--baseline_deaths_self_harm   "G:/Shared drives/Wellcome Trust Project Data/1_preprocess/UrbanCoolingModel/OfficialWorkingInputs/health_rasters_10m_bng/x60_x84_intentional_self_harm_bng10m_2021.tif" ^
     --out_dir "G:/Shared drives/Wellcome Trust Project Data/2_postprocess_intermediate/UCM_official_runs/health_output_s0_s42_2050_2050" ^
     --n_draws 100
 
