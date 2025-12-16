445e8a67f6f20bd7971f00b68e62ae23ced7ce85
Yingjie Li <yingjieli.edu@gmail.com>
2025-12-15 16:51:07 -0800

    saved functions

---
+++ code/func_packages.R
@@ -0,0 +1,12 @@
+
+
+#' commonly used R packages
+library(here)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(ggplot2)
+
+ymd   <- format(Sys.time(), "%Y%m%d"); ymd
