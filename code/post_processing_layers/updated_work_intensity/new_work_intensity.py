033e6ba8f54e7640920071848164b8cc91cd6eec
empavia <59708667+empavia@users.noreply.github.com>
2026-04-14 13:52:04 -0700

    update new work intensity script to accept extra variables

--- code/post_processing_layers/updated_work_intensity/new_work_intensity.py
+++ code/post_processing_layers/updated_work_intensity/new_work_intensity.py
@@ -30,16 +30,20 @@ def calculate_work_intensity(target_raster, wbgt_raster, alpha1, alpha2):
 
 # To loop over datasets
 path = "G:\\Shared drives\\Wellcome Trust Project Data\\2_postprocess_intermediate\\UCM_official_runs"
+# enter name of scenario for paths
+scenariofolder = "scenario520" 
+scenario = "scenario520" #name in files, either scenario or scenario##
 
 # Set the standard variables
 alpha1 = 30.94
 alpha2 = 16.64
-variables = np.array([[22.0, 2.0, 55.0], [22.0, 5.0, 55.0], [25.0, 2.0, 45.0],
-                      [25.0, 5.0, 45.0]])
-#variables = np.array([[28, 2, 45], [28, 5, 45]]) # these are separate as they don't have decimals in the main files
+#variables = np.array([[22.0, 2.0, 55.0], [22.0, 5.0, 55.0], [25.0, 2.0, 45.0],
+#                      [25.0, 5.0, 45.0]])
+variables = np.array([[22, 2, 55], [22, 5, 55], [25, 2, 45],
+                      [25, 5, 45], [28, 2, 45], [28, 5, 45]]) # these are separate as they don't have decimals in the main files
 for temp, uhi, hum in variables:
   # set the temperatures and UHI values in the inputs and outputs
-  target_raster = os.path.join(path, f'new_work_intensity/scenario0/high_work_productivity_{temp}deg_{uhi}uhi_{hum}hum.tif')
-  wbgt_raster = os.path.join(path, f'scenario0/work_and_energy_runs/intermediate/wbgt_london_scenario_{temp}deg_{uhi}uhi_{hum}hum_energy_productivity.tif')
+  target_raster = os.path.join(path, f'new_work_intensity/{scenariofolder}/high_work_productivity_{temp}deg_{uhi}uhi_{hum}hum.tif')
+  wbgt_raster = os.path.join(path, f'{scenariofolder}/work_and_energy_runs/intermediate/wbgt_london_{scenario}_{temp}deg_{uhi}uhi_{hum}hum_energy_productivity.tif')
   # run the new calculation in a loop
   calculate_work_intensity(target_raster, wbgt_raster, alpha1, alpha2)
